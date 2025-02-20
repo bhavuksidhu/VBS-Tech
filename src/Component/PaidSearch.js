bbc975b6c1f51d74e6b026a2660176f1a84477b3
Bhavuk Sidhu <bhavuksidhu@gmail.com>
2025-02-20 17:58:07 +0530

    added portfolio

--- src/Component/PaidSearch.js
+++ src/Component/PaidSearch.js
@@ -5,6 +5,14 @@ import img1 from '../Images/Frame 33.png';
 import img2 from '../Images/Frame 37.png';
 import img3 from '../Images/Frame 38.png';
 import img4 from '../Images/Frame 39.png';
+import img5 from '../Images/canvas5.png';
+import img6 from '../Images/img6.png';
+import img7 from '../Images/img7.png';
+import img8 from '../Images/img8.png';
+import img9 from '../Images/img9.png';
+import img10 from '../Images/img10.png';
+import img11 from '../Images/img11.png';
+import img12 from '../Images/img12.png';
 import Footer from '../Footer/Footer';
 
 export default function PaidSearch() {
@@ -67,6 +75,70 @@ export default function PaidSearch() {
                             <Link to="https://canvascultures.com/" target="https://canvascultures.com/">
                                 <img src={img4} className='imgdivpr' /></Link>
 
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='row mt-5'>
+                        <div className='col-lg-6 col-md-6 col-sm-12'>
+                            <div className='divwd'>
+                            <Link to="https://canvascultures.com/" target="https://canvascultures.com/"> 
+                                <img src={img5} className='imgdivpr' /></Link>
+
+                            </div>
+                        </div>
+                        <div className='col-lg-6 col-md-6 col-sm-12 arr'>
+                            <div className='divwd'>
+                            <Link to="https://collidedigital.com/" target="https://collidedigital.com/">
+                                <img src={img6} className='imgdivpr' /></Link>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='row mt-5'>
+                        <div className='col-lg-6 col-md-6 col-sm-12'>
+                            <div className='divwd'>
+                            <Link to="https://www.emerson.com/en-us/commercial-residential/grind2energy-food-waste-solution" target="https://www.emerson.com/en-us/commercial-residential/grind2energy-food-waste-solution"> 
+                                <img src={img7} className='imgdivpr' /></Link>
+
+                            </div>
+                        </div>
+                        <div className='col-lg-6 col-md-6 col-sm-12 arr'>
+                            <div className='divwd'>
+                            <Link to="https://www.stepbuildingsupplies.co.za/" target="https://www.stepbuildingsupplies.co.za/">
+                                <img src={img8} className='imgdivpr' /></Link>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='row mt-5'>
+                        <div className='col-lg-6 col-md-6 col-sm-12'>
+                            <div className='divwd'>
+                            <Link to="https://new.trystoryboard.com/" target="https://new.trystoryboard.com/"> 
+                                <img src={img9} className='imgdivpr' /></Link>
+
+                            </div>
+                        </div>
+                        <div className='col-lg-6 col-md-6 col-sm-12 arr'>
+                            <div className='divwd'>
+                            <Link to="https://www.bigcattravelinsurance.com/" target="https://www.bigcattravelinsurance.com/">
+                                <img src={img10} className='imgdivpr' /></Link>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='row mt-5'>
+                        <div className='col-lg-6 col-md-6 col-sm-12'>
+                            <div className='divwd'>
+                            <Link to="https://shopkaire.com/" target="https://shopkaire.com/"> 
+                                <img src={img11} className='imgdivpr' /></Link>
+
+                            </div>
+                        </div>
+                        <div className='col-lg-6 col-md-6 col-sm-12 arr'>
+                            <div className='divwd'>
+                            <Link to="https://shegerianconniff.com/" target="https://shegerianconniff.com/">
+                                <img src={img12} className='imgdivpr' /></Link>
                             </div>
                         </div>
 
