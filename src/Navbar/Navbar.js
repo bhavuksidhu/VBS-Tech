61125cc0320dfc2e9deef54dd544de8fd071d9cc
Bhavuk Siddhu <bhavuksidhu@gmail.com>
2025-02-21 13:47:08 +0530

    Nav bar changes

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -15,80 +15,77 @@ export default function Navbar({ cart }) {
   };
 
   return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-custom">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            <img src={img1} className="ms-3 mb-2" alt="Logo" style={{ width: "100px" }} />
-          </a>
-
-          <div className='d-flex hww'>
-            <NavLink className="nav-link change text-light btn1 ms-4 mt-2 btnn" to="/contactus" style={{ backgroundColor: "black" }}>
-              Contact Us
-            </NavLink>
-            <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" style={{ border: "none" }} onClick={toggleOffcanvas}>
+    <div>
+      <nav className="navbar navbar-expand-lg back-groundd">
+        <div className="container d-flex justify-content-between div1">
+        <NavLink  exact to="/">
+          <img src={img1} className="logoImg"  alt="Logo" />
+          </NavLink>
+        
+          <div className='d-flex navbaricon'>
+            <NavLink className="nav-link change text-light btn1 ms-4 mt-2 btnn" style={{ backgroundColor: "black" }} to="/contactus" activeClassName="active" onClick={closeOffcanvas}>Contact Us</NavLink>
+            <button className="navbar-toggler" type="button" onClick={toggleOffcanvas} aria-expanded={isOffcanvasOpen ? "true" : "false"}>
               <span className="navbar-toggler-icon"></span>
             </button>
           </div>
 
-          <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-            <ul className="navbar-nav mb-2 mb-lg-0">
-              <li className="nav-item ms-4 mt-2">
-                <NavLink className="nav-link change text-dark" to="/" end style={({ isActive }) => ({ color: isActive ? 'black' : '' })}>Home</NavLink>
+          <div className={`collapse navbar-collapse ${isOffcanvasOpen ? 'show' : ''}`} id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gapes">
+              <li className="nav-item">
+                <NavLink className="nav-link" exact to="/" activeClassName="active" onClick={closeOffcanvas}>Home</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/socialmedia" style={({ isActive }) => ({ color: isActive ? 'black' : '' })} onClick={closeOffcanvas}>About</NavLink>
+                <NavLink className="nav-link" to="/socialmedia" activeClassName="active" onClick={closeOffcanvas}>About</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/paidsearch" style={({ isActive }) => ({ color: isActive ? 'black' : '' })} onClick={closeOffcanvas}>Project</NavLink>
+                <NavLink className="nav-link" to="/paidsearch" activeClassName="active" onClick={closeOffcanvas}>Project</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/webdesign" style={({ isActive }) => ({ color: isActive ? 'black' : '' })} onClick={closeOffcanvas}>Service</NavLink>
+                <NavLink className="nav-link" to="/webdesign" activeClassName="active" onClick={closeOffcanvas}>Service</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/merkto" style={({ isActive }) => ({ color: isActive ? 'black' : '' })} onClick={closeOffcanvas}>Blog</NavLink>
+                <NavLink className="nav-link" to="/merkto" activeClassName="active" onClick={closeOffcanvas}>Blog</NavLink>
               </li>
-              <li className="nav-item ms-4 mt-2">
-                <NavLink className="nav-link change text-light" to="/contactus" style={{ backgroundColor: "black" }}>
-                  Contact Us
-                </NavLink>
+              <li className="nav-item">
+                <NavLink className="nav-link "  to="/contactus" activeClassName="active" onClick={closeOffcanvas}>Contact Us</NavLink>
               </li>
             </ul>
           </div>
         </div>
       </nav>
 
-      {/* Offcanvas menu */}
       <div className={`offcanvas offcanvas-start ${isOffcanvasOpen ? 'show' : ''}`} tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
         <div className="offcanvas-header">
-          <NavLink exact to="/">
-            <img src={img1} className="logoImg" alt="Logo" />
+          <NavLink  exact to="/">
+          <img src={img1} className="logoImg" alt="Logo" />
           </NavLink>
+        
           <button type="button" className="btn-close" onClick={toggleOffcanvas} aria-label="Close"></button>
         </div>
         <div className="offcanvas-body">
           <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
             <li className="nav-item">
-              <NavLink className="nav-link" exact to="/" style={({ isActive }) => ({ color: isActive ? 'black' : '' })} onClick={closeOffcanvas}>Home</NavLink>
+              <NavLink className="nav-link" exact to="/" activeClassName="active" onClick={closeOffcanvas}>Home</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/socialmedia" style={({ isActive }) => ({ color: isActive ? 'black' : '' })} onClick={closeOffcanvas}>About</NavLink>
+              <NavLink className="nav-link" to="/socialmedia" activeClassName="active" onClick={closeOffcanvas}>About</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/paidsearch" style={({ isActive }) => ({ color: isActive ? 'black' : '' })} onClick={closeOffcanvas}>Project</NavLink>
+              <NavLink className="nav-link" to="/paidsearch" activeClassName="active" onClick={closeOffcanvas}>Project</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/webdesign" style={({ isActive }) => ({ color: isActive ? 'black' : '' })} onClick={closeOffcanvas}>Service</NavLink>
+              <NavLink className="nav-link" to="/webdesign" activeClassName="active" onClick={closeOffcanvas}>Service</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/merkto" style={({ isActive }) => ({ color: isActive ? 'black' : '' })} onClick={closeOffcanvas}>Blog</NavLink>
+              <NavLink className="nav-link" to="/merkto" activeClassName="active" onClick={closeOffcanvas}>Blog</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/contactus" style={({ isActive }) => ({ color: isActive ? 'black' : '' })} onClick={closeOffcanvas}>Contact Us</NavLink>
+            <NavLink className="nav-link"  to="/contactus" activeClassName="active" onClick={closeOffcanvas}>ContactUs</NavLink>
             </li>
+            
           </ul>
         </div>
       </div>
-    </>
+    </div>
   );
 }
